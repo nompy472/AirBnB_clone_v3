652245058b28c85da2f2ba09126b2ec00a9d488a
daniell-olaitan <daniell.olaitan@gmail.com>
2024-01-31 00:38:07 +0100

    Implement API for city and amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,11 +4,9 @@ from models.amenity import Amenity
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/amenity/all_amenities.yml')
 def get_amenities():
     """Retrieve list of all amenities"""
     all_amenities = storage.all(Amenity).values()
@@ -20,7 +18,6 @@ def get_amenities():
 
 @app_views.route('/amenities/<amenity_id>/', methods=['GET'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/get_amenity.yml', methods=['GET'])
 def get_amenity(amenity_id):
     """ Retrieve  amenity """
     amenity = storage.get(Amenity, amenity_id)
@@ -32,7 +29,6 @@ def get_amenity(amenity_id):
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/delete_amenity.yml', methods=['DELETE'])
 def delete_amenity(amenity_id):
     """Delete  amenity  Object"""
 
@@ -48,7 +44,6 @@ def delete_amenity(amenity_id):
 
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/amenity/post_amenity.yml', methods=['POST'])
 def post_amenity():
     """Create an amenity"""
     if not request.get_json():
@@ -65,7 +60,6 @@ def post_amenity():
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/put_amenity.yml', methods=['PUT'])
 def put_amenity(amenity_id):
     """Updating an amenity"""
     if not request.get_json():
--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,29 +5,27 @@ from models.state import State
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
-@swag_from('documentation/city/cities_by_state.yml', methods=['GET'])
 def get_cities(state_id):
     """
-    Retrieves the list of all cities 
+    Retrieves the list of all cities
     Objects of a specific State, or a specific city
     """
-    list_cities = []
+    cities = []
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    for city in state.cities:
-        list_cities.append(city.to_dict())
 
-    return jsonify(list_cities)
+    for city in state.cities:
+        cities.append(city.to_dict())
+
+    return jsonify(cities)
 
 
 @app_views.route('/cities/<city_id>/', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/city/get_city.yml', methods=['GET'])
 def get_city(city_id):
     """
     Retrieves a specific city
@@ -35,11 +33,11 @@ def get_city(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
+
     return jsonify(city.to_dict())
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
-@swag_from('documentation/city/delete_city.yml', methods=['DELETE'])
 def delete_city(city_id):
     """
     Deleting a city based on id provided
@@ -56,19 +54,18 @@ def delete_city(city_id):
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
-@swag_from('documentation/city/post_city.yml', methods=['POST'])
 def post_city(state_id):
     """Creating a City
     """
     state = storage.get(State, state_id)
+    data = request.get_json()
     if not state:
         abort(404)
-    if not request.get_json():
+    if not data:
         abort(400, description="Not a JSON")
     if 'name' not in request.get_json():
         abort(400, description="Missing name")
 
-    data = request.get_json()
     instance = City(**data)
     instance.state_id = state.id
     instance.save()
@@ -76,7 +73,6 @@ def post_city(state_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/city/put_city.yml', methods=['PUT'])
 def put_city(city_id):
     """
     Updating a City
