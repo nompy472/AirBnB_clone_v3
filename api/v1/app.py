d301e4e0c7df6e1baa0a1aed76802d9ec3584714
daniell-olaitan <daniell.olaitan@gmail.com>
2024-02-04 10:41:36 +0100

    Undo the previous commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -37,5 +37,5 @@ if __name__ == "__main__":
     if not host:
         host = '0.0.0.0'
     if not port:
-        port = '5050'
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
