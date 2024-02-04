346ccf2f6c269c6b06fadea1dc6be9f5e1ffcd40
daniell-olaitan <daniell.olaitan@gmail.com>
2024-02-04 10:37:47 +0100

    Debug

--- api/v1/app.py
+++ api/v1/app.py
@@ -37,5 +37,5 @@ if __name__ == "__main__":
     if not host:
         host = '0.0.0.0'
     if not port:
-        port = '5000'
+        port = '5050'
     app.run(host=host, port=port, threaded=True)
